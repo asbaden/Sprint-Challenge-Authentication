3b869873b9aa77b75a19f7ff7e343e95e948af2c
asbaden <gusbaden96@gmail.com>
2020-02-28 13:12:18 -0800

    mvp

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,60 +1,59 @@
-const router = require('express').Router();
+const router = require("express").Router();
 const Users = require("../users/users-model");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
 function generateToken(user) {
-	const payload = {
-		sub: user.id,
-		username: user.username
-	};
+  const payload = {
+    sub: user.id,
+    username: user.username
+  };
 
-	const options = {
-		expiresIn: 60 * 60
-	};
+  const options = {
+    expiresIn: 60 * 60
+  };
 
-	const token = jwt.sign(
-		payload,
-		process.env.JWT_SECRETE || "thesecret",
-		options
-	);
+  const token = jwt.sign(
+    payload,
+    process.env.JWT_SECRETE || "thesecret",
+    options
+  );
 
-	return token;
+  return token;
 }
 
-
-router.post('/register', (req, res) => {
+router.post("/register", (req, res) => {
   // implement registration
   req.body.password = bcrypt.hashSync(req.body.password, 10);
-	Users.add(req.body)
-		.then(saved => {
-			res.status(201).json(saved);
-		})
-		.catch(error => {
-			res.status(500).json(error);
-		});
+  Users.add(req.body)
+    .then(saved => {
+      res.status(201).json(saved);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
 });
 
-router.post('/login', (req, res) => {
+router.post("/login", (req, res) => {
   // implement login
   let { username, password } = req.body;
 
-	Users.findBy({ username })
-		.first()
-		.then(user => {
-			if (user && bcrypt.compareSync(password, user.password)) {
-				const token = generateToken(user);
-				res.status(200).json({
-					message: `Welcome ${user.username}!`,
-					token
-				});
-			} else {
-				res.status(401).json({ message: "You shall not pass!!" });
-			}
-		})
-		.catch(error => {
-			res.status(500).json({ message: error.message }); // funky {}
-		});
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user);
+        res.status(200).json({
+          message: `Welcome ${user.username}!`,
+          token
+        });
+      } else {
+        res.status(401).json({ message: "You shall not pass!!" });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({ message: error.message }); 
+    });
 });
 
 module.exports = router;
